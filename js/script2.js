887099ff9cb0ab7432a97fbe323319f19b8bbb15
tomeitor <tomycarnezis@gmail.com>
2023-07-24 23:11:19 -0300

    preentrega numero 3

--- js/script2.js
+++ js/script2.js
@@ -85,7 +85,7 @@ for (let index = 0; index < 5; index++) {
 
 //                     Preentrega n°2:
 
-let lineasTrenes = {
+/* let lineasTrenes = {
   roca: [
     {
       estacionNro: 1,
@@ -100,13 +100,13 @@ let lineasTrenes = {
       tardaEnMin: 4,
     },
     {
-      estacionNro: 1,
+      estacionNro: 3,
       hasta: "d santillan y m kosteki",
       precio: 40,
       tardaEnMin: 7,
     },
     {
-      estacionNro: 1,
+      estacionNro: 4,
       hasta: "gerli",
       precio: 45,
       tardaEnMin: 10,
@@ -192,7 +192,7 @@ if (estacionesDeLinea) {
 
   if (resultado) {
     console.log("Información de la estación seleccionada: ");
-    console.log(resultado);
+    console.table(resultado);
   } else {
     console.log(
       'La estación "' + estacionSeleccionada + '" no se encuentra en esta linea'
@@ -201,3 +201,155 @@ if (estacionesDeLinea) {
 } else {
   console.log('La línea de tren "' + lineaSeleccionada + '" no existe');
 }
+
+let nombre1 = ["juan", "pedro"];
+let nombre2 = ["pepito", "pepa"];
+
+let nombre = nombre1 + nombre2;
+
+console.log(nombre); */
+
+/*
+// ctrl+k+c
+ctrl+k+u
+ */
+
+//                     Preentrega n°3:
+
+let lineasTrenes = {
+  roca: [
+    {
+      estacionNro: 1,
+      hasta: "constitucion",
+      precio: 0,
+      tardaEnMin: 0,
+    },
+    {
+      estacionNro: 2,
+      hasta: "yrigoyen",
+      precio: 35,
+      tardaEnMin: 4,
+    },
+    {
+      estacionNro: 3,
+      hasta: "d santillan y m kosteki",
+      precio: 40,
+      tardaEnMin: 7,
+    },
+    {
+      estacionNro: 4,
+      hasta: "gerli",
+      precio: 45,
+      tardaEnMin: 10,
+    },
+    {
+      estacionNro: 5,
+      hasta: "lanus",
+      precio: 50,
+      tardaEnMin: 15,
+    },
+    {
+      estacionNro: 6,
+      hasta: "escalada",
+      precio: 50,
+      tardaEnMin: 20,
+    },
+    {
+      linea: "roca",
+      estacionNro: 7,
+      hasta: "banfield",
+      precio: 55,
+      tardaEnMin: 25,
+    },
+    {
+      linea: "roca",
+      estacionNro: 8,
+      hasta: "lomas",
+      precio: 55,
+      tardaEnMin: 30,
+    },
+    {
+      linea: "roca",
+      estacionNro: 9,
+      hasta: "temperley",
+      precio: 60,
+      tardaEnMin: 35,
+    },
+    {
+      linea: "roca",
+      estacionNro: 10,
+      hasta: "adrogue",
+      precio: 60,
+      tardaEnMin: 40,
+    },
+    {
+      linea: "roca",
+      estacionNro: 11,
+      hasta: "burzaco",
+      precio: 65,
+      tardaEnMin: 45,
+    },
+  ],
+  sarmiento: [
+    {
+      estacionNro: 1,
+      hasta: "moreno",
+      precio: 30,
+      tardaEnMin: 10,
+    },
+    {
+      estacionNro: 2,
+      hasta: "merlo",
+      precio: 35,
+      tardaEnMin: 15,
+    },
+    // ...
+    //muchas lineas y estaciones...
+  ],
+};
+
+let formLinea = document.getElementById("formLinea");
+let formEstacion = document.getElementById("formEstacion");
+
+formLinea.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+  formLinea.style.display = "none";
+  formEstacion.style.display = "block";
+
+  let lineaSeleccionada = document.getElementById("lineaTren").value;
+  lineaSeleccionada = lineaSeleccionada.toLowerCase();
+
+  let estacionesDeLinea = lineasTrenes[lineaSeleccionada];
+
+  if (estacionesDeLinea) {
+    formEstacion.addEventListener("submit", function (evento) {
+      evento.preventDefault();
+
+      let estacionSeleccionada = document.getElementById("estacionTren").value;
+      estacionSeleccionada = estacionSeleccionada.toLowerCase();
+
+      let resultado = estacionesDeLinea.find(
+        (estacion) => estacion.hasta.toLowerCase() === estacionSeleccionada
+      );
+
+      if (resultado) {
+        console.log("Información de la estación seleccionada: ");
+        console.table(resultado);
+        let historial = localStorage.setItem(
+          "historial",
+          JSON.stringify(resultado)
+        );
+      } else {
+        console.log(
+          'La estación "' +
+            estacionSeleccionada +
+            '" no se encuentra en esta linea'
+        );
+      }
+    });
+  } else {
+    console.log('La línea de tren "' + lineaSeleccionada + '" no existe');
+  }
+});
+
+//se guarda la informacion como historial en el localStorage
